f0364864b34b8d2522627996fed373d94f0fddcc
Anas <135501348+anasfanani@users.noreply.github.com>
2024-06-08 08:28:49 +0000

    Better implementation

--- scripts/files/autoboot.sh
+++ scripts/files/autoboot.sh
@@ -1,22 +1,32 @@
 #!/system/bin/sh
-
-CAPACITY=$(cat /sys/class/power_supply/battery/capacity)
-MIN_CAPACITY=5
-if [ $CAPACITY -gt $MIN_CAPACITY ]
-then
-    sleep 1
+reboot_device() {
     setprop ro.bootmode "normal"
     setprop sys.powerctl "reboot"
-
-else
-    while [[ $CAPACITY -le $MIN_CAPACITY ]]; do
-        sleep 60  
-        CAPACITY=$(cat /sys/class/power_supply/battery/capacity)  
-        if [ $CAPACITY -gt $MIN_CAPACITY ]
-        then
-            sleep 1
-            setprop ro.bootmode "normal"
-            setprop sys.powerctl "reboot"
-        fi
-    done
+    reboot
+}
+if [ "$(getprop autoboot)" != "1" ]; then
+    setprop autoboot 1
+    if [ ! -f /sys/class/power_supply/battery/capacity ]; then
+        reboot_device
+    else
+        # Minimum battery capacity to boot
+        MIN_CAPACITY=5
+        # Maximum number of attempts to check battery capacity
+        MAX_ATTEMPTS=10
+        c=0
+        while [ $c -lt $MAX_ATTEMPTS ]; do
+            CAPACITY=$(cat /sys/class/power_supply/battery/capacity)
+            case $CAPACITY in
+                ''|*[!0-9]*) CAPACITY=100 ;; # Set CAPACITY to 100 if it's not a number
+            esac
+            # If battery capacity is greater than minimum capacity or reached maximum attempts
+            if [ "$CAPACITY" -gt $MIN_CAPACITY ] || [ $c -eq $((MAX_ATTEMPTS-1)) ]; then
+                reboot_device
+                exit
+            fi
+            # Wait for battery to charge
+            sleep 10
+            c=$((c+1))
+        done
+    fi
 fi
